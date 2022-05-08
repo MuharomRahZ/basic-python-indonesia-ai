c1dc8e9203a23446a4f2d978b4a4fbe9307274b7
MuharomRahZ <rahzaki05@gmail.com>
2022-05-08 14:12:59 +0800

    add modif final project

--- Final-Project/final_project.py
+++ Final-Project/final_project.py
@@ -8,17 +8,18 @@ password_sender = input("Masukkan password pengirim: ")
 
 
 context = ssl.create_default_context() #membuat protokol pengiriman email dengan default
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.ehlo()
-    server.login(gmail_sender, password_sender)
-    with open(r'C:\Users\ZAKI\Basic-Python-Indonesia-AI\python files\final_3\receiver_list.txt', 'r') as file:
-        gmail_receiver = file.read().replace('\n', ',')
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: #mengatur profil protokol server
+    server.ehlo() #menetapkan localhost
+    server.login(gmail_sender, password_sender) #menetapkan email & pass user/sender
+    with open(r'C:\Users\ZAKI\Basic-Python-Indonesia-AI\basic-python-indonesia-ai\Final-Project\receiver_list.txt', 'r') as file: #membaca file rceiver
+        gmail_receiver = file.read().replace('\n', ',') #mengatur variabel sebagai receiver
 
-        sent_from = gmail_sender
-        sent_to = gmail_receiver
-        sent_subject = input(str("Masukkan subjek email: "))
-        sent_body = input(str("Masukkan isi pesan email: "))
+        sent_from = gmail_sender #variabel sender
+        sent_to = gmail_receiver #variabel receiver
+        sent_subject = input(str("Masukkan subjek email: ")) #variabel subject
+        sent_body = input(str("Masukkan isi pesan email: ")) #variabel body
 
+        #deklarasi body email
         message = """\
         from: %s
         to: %s
